b80408922ce7ef55f93b2a639f19876cedd49f5c
norikoda <kodaka997531@gmail.com>
2022-06-14 22:58:27 +0900

    ログイン、ログアウト時の表示変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
 
-  validates :nickname, presence: true, uniqueness: true
+  validates :nickname, presence: true
   # @を含むこと・deviseのデフォルト実装
   validates :email,    uniqueness: true
   # 全角ひらがな、全角カタカナ、漢字
