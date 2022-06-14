282efd700389fbef1b83592db2e11ee1ee64638c
norikoda <kodaka997531@gmail.com>
2022-06-14 22:18:29 +0900

    katakanaからkanaに修正

--- db/migrate/20220614112355_devise_create_users.rb
+++ db/migrate/20220614112355_devise_create_users.rb
@@ -9,8 +9,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :encrypted_password,     null: false, default: ""
       t.string :last_name,              null: false
       t.string :first_name,             null: false
-      t.string :last_name_katakana,     null: false
-      t.string :first_name_katakana,    null: false
+      t.string :last_name_kana,     null: false
+      t.string :first_name_kana,    null: false
       t.date   :birthday,               null: false
 
       ## Recoverable
