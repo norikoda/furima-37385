255e1bcd3642f26738467eada6346791031cc4a5
norikoda <kodaka997531@gmail.com>
2022-06-15 21:54:11 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
-
+  with_options presence: true do
+    validates :user
+    validates :name
+    validates :price
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
 end
