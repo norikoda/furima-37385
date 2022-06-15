6d75f68913c5b7fb2d1eacdf3084e733c518a2c6
norikoda <kodaka997531@gmail.com>
2022-06-15 23:13:31 +0900

    ActiveHashのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :shipping_date
+
   belongs_to :user
   has_one_attached :image
+
+  with_options numericality: { other_than: 1 } do
+    validates :prefecture_id
+    validates :category_id
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :shipping_date_id
+  end   
 
   with_options presence: true do
     validates :user
