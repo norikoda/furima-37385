392bcb97911be552ba526fc2b7272c1b18e8defe
norikoda <kodaka997531@gmail.com>
2022-06-16 01:09:03 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,8 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
   with_options numericality: { other_than: 1 } do
     validates :prefecture_id
     validates :category_id
