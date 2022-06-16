ae4f3c030ca4cb370ec6b1424cd928604d203db8
norikoda <kodaka997531@gmail.com>
2022-06-16 19:45:46 +0900

    priceのバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   with_options numericality: { other_than: 1 } do
     validates :prefecture_id
