7b4241309b7e8a46e0cba0f1cd1ca42ee2a27422
norikoda <kodaka997531@gmail.com>
2022-06-18 20:46:43 +0900

    商品購入ページへの遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,25 @@
 class OrdersController < ApplicationController
 
-
-
   def index
+    @item = Item.find(params[:item_id])
     @order_pay = OrderPay.new
   end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_pay = OrderPay.new(order_params)
+   if @order_pay.valid?
+    @order_pay.save
+   else 
+     render :index
+   end
+  end
+
+private
+def order_params
+  params.require(:order_pay).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(
+    user_id: current_user.id, item_id: params[:item_id]
+  )
+end
 
 end
